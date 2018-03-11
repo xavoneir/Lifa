650b7ec31176a4e707d20511d1b001efba0031e5
NotaGood-Username <bschan9228@gmail.com>
2018-03-10 21:51:00 -0800

    Update Lifa.py

--- Lifa.py
+++ Lifa.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 import time
 
-joincode = "157604"                   #Join code for Quizlet.live
+joincode = "724302"                   #Join code for Quizlet.live
 name = "Account Name"                       #Name for Quizlet.live
 initial = "_"                    #In case of duplicates, Quizlet may ask for an initial
 
 with open('quizlet.txt', 'r') as file:
     page = file.read().split('\n')
+print(page)
 
-driver = webdriver.Firefox(executable_path=r'C:\\Users\\Chan\\Desktop\\Python\\Gen\\SelDrivers\\geckodriver.exe')
+driver = webdriver.Firefox(executable_path=r'PATH\\TO\\geckodriver.exe')
 
 driver.get('https://quizlet.com/live')
 driver.find_element_by_class_name('UIInput-input').send_keys(joincode)
@@ -22,15 +23,25 @@ try:
     driver.find_element_by_class_name('UIButton-wrapper').click()
 except:
     pass
+print('Ready!')
 
 
+# POSSIBLE ANSWERS WIP, QUESTION CURRENTLY WORKING
+while True:
 
-while(True):
-    try:
-        question = driver.find_element_by_class_name('StudentPrompt-inner').text.encode('utf-8')        #Question
-        print(question)
+    while True:
+        try:
+            possible_answer = driver.find_element_by_class_name('StudentTermGroup-terms').text.encode('utf-8')  #All possible answers MAIN
+            print('possible_answer: ' + possible_answer)
+            if possible_answer != '':
+                break
+        except:
+            pass
 
-        possible_answer = driver.find_element_by_class_name('StudentTermGroup-terms').text.encode('utf-8')  #All possible answers
-        print(possible_answer)
-    except:
-        pass
+    while True:
+        question = driver.find_element_by_class_name('StudentPrompt-inner').text.encode('utf-8')  # Question
+        print('question: ' + question)
+        real_possible_answer = driver.find_element_by_class_name('StudentTermGroup-terms').text.encode('utf-8')  #All possible answers CHECK
+
+        if not (all(x in possible_answer for x in real_possible_answer.split())):
+            possible_answer = (driver.find_element_by_xpath('/html/body/div[2]/main/div/div/div[1]/div/div/span/div/div[' + str(num) + ']/div/div[2]').text)  # My Answers
