bfc5b04d92ff7303783eed37ece61b9e824e1bb3
NotaGood-Username <bschan9228@gmail.com>
2018-03-10 20:59:41 -0800

    Update Lifa.py

--- Lifa.py
+++ Lifa.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 import time
 
-joincode = "514034"                   #Join code for Quizlet.live
+joincode = "828520"                   #Join code for Quizlet.live
 name = "Account Name"                       #Name for Quizlet.live
 initial = "_"                    #In case of duplicates, Quizlet may ask for an initial
+
+with open('quizlet.txt', 'r') as file:
+    page = file.read().split('\n')
 
 driver = webdriver.Firefox(executable_path=r'C:\\Users\\Chan\\Desktop\\Python\\Gen\\SelDrivers\\geckodriver.exe')
 
@@ -19,3 +22,14 @@ try:
     driver.find_element_by_class_name('UIButton-wrapper').click()
 except:
     pass
+
+
+
+while(True):
+    try:
+        question = driver.find_element_by_class_name('StudentPrompt-inner').text.encode('utf-8')
+        print(question)
+        possible_answer = driver.find_element_by_class_name('StudentTermGroup-terms').text.encode('utf-8')
+        print(answer)
+    except:
+        pass
