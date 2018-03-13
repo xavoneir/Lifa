604ffefe9e98b7214960b19f86cf375c356d39ca
NotaGood-Username <bschan9228@gmail.com>
2018-03-12 21:35:17 -0700

    Questions and Answers are now found

--- Lifa.py
+++ Lifa.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-joincode = "659160"                   #Join code for Quizlet.live
+joincode = "864712"                   #Join code for Quizlet.live
 name = "Account Name"                       #Name for Quizlet.live
 initial = "_"                    #In case of duplicates, Quizlet may ask for an initial
 
@@ -25,16 +25,15 @@ except:
     pass
 print('Ready!')
 
-
-# POSSIBLE ANSWERS CURRENTLY NOT WORKING, QUESTION SHOULD BE WORKING
 while True:
     num = 1
-
+    print('Ok!')
     while True:
         try:
-            possible_answer = driver.find_element_by_xpath('/html/body/div[2]/main/div/div/div[1]/div/div/span/div/div[' + str(num) + ']/div/div[2]').text.encode('utf-8')    #All possible answers MAIN
-            print('still at ' + possible_answer)
-            if possible_answer != '':                                     #/html/body/div[2]/main/div/div/div[1]/div/div/span/div/div[2]/div/div[2] error right
+            print('num: ' + str(num))
+            possible_answer = str(driver.find_element_by_xpath('/html/body/div[2]/main/div/div/div[1]/div/div/span/div/div[' + str(num) + ']/div/div[2]').text.encode('utf-8'))    #All possible answers MAIN
+            print('still at: ' + possible_answer)
+            if possible_answer != 'b\'\'':                                     #/html/body/div[2]/main/div/div/div[1]/div/div/span/div/div[2]/div/div[2] error right
                 print('possible_answer init: ' + possible_answer)         #/html/body/div[2]/main/div/div/div[1]/div/div/span/div/div[1]/div/div[2]
                 break
 
@@ -42,15 +41,12 @@ while True:
                 num = 1
             else:
                 num += 1
-            print('num: ' + num)
-
         except:
             pass
 
     while True:
         question = driver.find_element_by_class_name('StudentPrompt-inner').text.encode('utf-8')  # Question
-        print('question: ' + question)
+        print('question: ' + str(question))
         real_possible_answer = driver.find_element_by_class_name('StudentTermGroup-terms').text.encode('utf-8')  #All possible answers CHECK
-
         if not (all(x in possible_answer for x in real_possible_answer.split())):
             possible_answer = driver.find_element_by_xpath('/html/body/div[2]/main/div/div/div[1]/div/div/span/div/div[' + str(num) + ']/div/div[2]').text.encode('utf-8')  # My Answers
